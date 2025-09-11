3620ce248f3fbc3d9e23c3cf80190d4a9dd4bb3d
Abhinav <abhinavj2114@gmail.com>
2025-09-11 12:28:55 +0530

    password reset mail error sorted

--- zeuzcrypto_server/zeuzcryptoserver/zeuzcryptoserver/settings/base.py
+++ zeuzcrypto_server/zeuzcryptoserver/zeuzcryptoserver/settings/base.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from decouple import config
 import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # -----------------------------------------------------------------------------
 # Security & Debug
