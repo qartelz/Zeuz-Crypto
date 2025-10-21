a9a1255ce9c398a2816d87e4098dc2752afaa778
Abhinav <abhinavj2114@gmail.com>
2025-10-21 14:31:09 +0530

    allowed host added

--- zeuzcrypto_server/zeuzcryptoserver/zeuzcryptoserver/settings/dev.py
+++ zeuzcrypto_server/zeuzcryptoserver/zeuzcryptoserver/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "zeuzcrypto.com", "www.zeuzcrypto.com", "13.51.47.77"]
 
 DATABASES = {
     "default": {
