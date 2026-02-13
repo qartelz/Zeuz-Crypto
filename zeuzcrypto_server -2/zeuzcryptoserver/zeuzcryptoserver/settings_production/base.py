39ede31d64fd65c3c03bf073976cded9befc5c87
Abhinav <abhinavj2114@gmail.com>
2026-02-13 18:23:27 +0530

    Hosting for railway5

--- zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/settings_production/base.py
+++ zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/settings_production/base.py
@@ -482,21 +482,25 @@ LOGGING = {
 
 # ROOT_URLCONF = "zeuzcryptoserver.urls"
 
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [BASE_DIR / "templates"],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # # TEMPLATES = [
 # #     {
 # #         "BACKEND": "django.template.backends.django.DjangoTemplates",
