37fe9cc25eb74fd48cbf6e79d5d4cdd817c3c00a
Abhinav <abhinavj2114@gmail.com>
2026-02-13 12:30:27 +0530

    Hosting for railway4

--- zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/asgi.py
+++ zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/asgi.py
@@ -5,10 +5,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zeuzcryptoserver.settings.prod")
 django.setup()
 
-from django.conf import settings
-print(f"DEBUG: DJANGO_SETTINGS_MODULE = {os.environ.get('DJANGO_SETTINGS_MODULE')}")
-print(f"DEBUG: DATABASES = {settings.DATABASES}")
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 application = ProtocolTypeRouter({
