f4e06b3b2d5d96e82271f5b50d60432bf6dd4a37
Abhinav <abhinavj2114@gmail.com>
2026-02-13 13:05:51 +0530

    Hosting for railway4

--- zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/settings.py
+++ zeuzcrypto_server -2/zeuzcryptoserver/zeuzcryptoserver/settings.py
@@ -74,11 +74,17 @@ WSGI_APPLICATION = "zeuzcryptoserver.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
+# ...
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:iDLijIdGtsgKjIeJxmUHDUDskdftytZX@shinkansen.proxy.rlwy.net:13821/railway",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
